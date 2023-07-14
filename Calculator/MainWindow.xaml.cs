68ee6e979c955cb4272ce35790d88f186b3f60b8
sirayuri <yosahanan@gmail.com>
2023-07-14 09:02:12 +0900

    四則演算をリリース

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calculator
 {
@@ -33,30 +21,82 @@ namespace Calculator
         {
             kekka.AppendText("The firest value is 0\n");
         }
-         
+
+        private void box_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!IsNumericInput(e.Text))
+            {
+                e.Handled = true;
+            }
+        }
+
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
-
-            if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control && e.Key == Key.P) //足し算
+            //MessageBox.Show(e.Key.ToString());
+            if (e.Key == Key.OemPlus) //足し算
             {
+                if (box.Text == "")
+                {
+                    box.Text = "0";
+                }
+
                 ans = ans + double.Parse(box.Text);
                 kekka.AppendText($"{ans}\n");
                 box.Text = "";
+                kekka.ScrollToEnd();
             }
 
-            if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control && e.Key == Key.S) //引き算
+            if (e.Key == Key.OemMinus) //引き算
             {
+                if(box.Text == "")
+                {
+                    box.Text = "0";
+                }
+
                 ans = ans - double.Parse(box.Text);
                 kekka.AppendText($"{ans}\n");
                 box.Text = "";
+                kekka.ScrollToEnd();
             }
 
+            if(e.Key == Key.Oem1 && Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+            {
+                if(box.Text == "")
+                {
+                    box.Text = "0";
+                }
 
+                ans = ans * double.Parse(box.Text);
+                kekka.AppendText($"{ans}\n");
+                box.Text = "";
+                kekka.ScrollToEnd();
+            }
+
+            if(e.Key == Key.OemQuestion && Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+            {
+                if(box.Text == "")
+                {
+                    box.Text ="0";
+                }
+
+                if (double.Parse(box.Text) == 0)
+                {
+                    kekka.AppendText("0除算はできません\n");
+                    return;
+                }
+
+                ans = ans / double.Parse(box.Text);
+                kekka.AppendText($"{ans}\n");
+                box.Text = "";
+                kekka.ScrollToEnd();
+            }
         }
 
-        private void box_TextChanged(object sender, TextChangedEventArgs e)
+        private bool IsNumericInput(string input)
         {
-            
+            // 入力文字列が数字かどうかを判定
+            return int.TryParse(input, out _);
         }
+
     }
 }
